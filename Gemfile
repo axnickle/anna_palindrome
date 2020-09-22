16e6e97125568a402cb7c3c53bed131f078c53c8
anickle <anickle@covermymeds.com>
2020-09-22 16:28:29 -0400

    Finish working and refactored palindrome method

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source "https://rubygems.org"
 
+git_source(:github) {|anna_palindrome| "https://git.innova-partners.com/anickle/anna_palindrome" }
 # Specify your gem's dependencies in anna_palindrome.gemspec
 gemspec
 
 gem "rake", "~> 12.0"
 gem "minitest", "~> 5.0"
+gem 'minitest-reporters', '1.2.0'
--- lib/anna_palindrome.rb
+++ lib/anna_palindrome.rb
@@ -1,6 +1,25 @@
 require "anna_palindrome/version"
+  # putting the palindrome detector in the gem.
+class String
 
-module AnnaPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns the letters in the string.
+  def letters
+    self.chars.select { |c| c.match(/[a-z]/i) }.join
+  end
+
+  private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    # self.letters.downcase
+    scan(/[a-z]/i).join.downcase
 end
+end
+
+# Ruby is smarte enough to apply the #scan
+  # to the string itself
